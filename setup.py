6a9b10f4e547935d440d0e2df8be82b239a68ac7
Anand Pranav N <npranav200@gmail.com>
2025-09-24 20:08:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Anand",
+    author_email="anand.nijagal@gmail.com"
+)
